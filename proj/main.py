9df69cbc71022e352b2015ed1c5f0fa4c15e8cbe
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-24 15:55:57 +0900

    Add dummy python package

---
+++ proj/main.py
@@ -0,0 +1,17 @@
+
+class Sample:
+    """Sample class."""
+    def __init__(self):
+        pass
+
+    def method(self, a):
+        """Sample methods.
+
+        Args:
+            a (int): input
+
+        Returns:
+            b (float): some value
+        """
+
+        return 0
