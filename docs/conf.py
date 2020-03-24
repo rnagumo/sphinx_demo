6f8959fef8f0d350f4bb761131fcf712f5e4a8c2
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-03-24 18:43:00 +0900

    Add docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,14 +14,14 @@ import sphinx_rtd_theme
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../proj/'))
+sys.path.insert(0, os.path.abspath('../proj/'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'demo'
-copyright = '2020, rnagumo'
-author = 'rnagumo'
+project = 'proj'
+copyright = '2020, rnaugmo'
+author = 'rnaugmo'
 
 
 # -- General configuration ---------------------------------------------------
@@ -37,7 +37,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,9 @@
-.. demo documentation master file, created by
-   sphinx-quickstart on Tue Mar 24 15:28:19 2020.
+.. proj documentation master file, created by
+   sphinx-quickstart on Tue Mar 24 18:29:07 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to demo's documentation!
+Welcome to proj's documentation!
 ================================
 
 .. toctree::
